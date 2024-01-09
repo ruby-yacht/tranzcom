89a826e647c3465a40aef3661bb9edef8d22a890
kidNeutrin0 <tasmuth@uwf.edu>
2024-01-08 20:59:39 -0600

    update

--- p5sketches/imageClassifier02/imageClassifier02.js
+++ p5sketches/imageClassifier02/imageClassifier02.js
@@ -12,12 +12,24 @@ console.error(error);
 }
 else {
 console.log(results);
+let lab = results[0].label;
+let prob =results[0].confidence;
+fill(255.75);
+textSize(50);
+text(lab,10, height-100);
+text(prob,10, height-150);
+createP(lab);
+createP(prob);
 }
 }
 
 function imageReady() {
    image(turbine, 0, 0, width, height);
 }
+
+
+
+
 
 function setup() {
 createCanvas(600, 480);
